eee9be736a2f32d791a63a8565566e5bae911bcf
Alejandro Proano <alejandro.proano@verizondigitalmedia.com>
2019-01-26 22:47:41 -0800

    adding few helper functions to define and display the sudoku board

---
+++ helper.py
@@ -0,0 +1,31 @@
+def cross(a, b):
+    return [s+t for s in a for t in b]
+
+def grid_values(grid):
+    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+
+    Args:
+        grid: Sudoku grid in string form, 81 characters long
+    Returns:
+        Sudoku grid in dictionary form:
+        - keys: Box labels, e.g. 'A1'
+        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+    """
+    if len(grid) != 81:
+        return {}
+    rows = 'ABCDEFGHI'
+    cols = '123456789'
+    return {rows[x//9]+cols[x%9]: grid[x] for x in range(81)}
+
+def display(values):
+    """
+    Display the values as a 2-D grid.
+    Input: The sudoku in dictionary form
+    Output: None
+    """
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)  
