5f677318c350bf390c62be89ba04f0c92074334e
Alejandro Proano <alejandro.proano@verizondigitalmedia.com>
2019-01-28 09:56:17 -0800

    funtions for remove and pick values

--- helper.py
+++ helper.py
@@ -2,20 +2,79 @@ def cross(a, b):
     return [s+t for s in a for t in b]
 
 def grid_values(grid):
-    """Convert grid string into {<box>: <value>} dict with '.' value for empties.
+    """Convert grid string into {<box>: <value>} dict with '123456789' value for empties.
 
     Args:
         grid: Sudoku grid in string form, 81 characters long
     Returns:
         Sudoku grid in dictionary form:
         - keys: Box labels, e.g. 'A1'
-        - values: Value in corresponding box, e.g. '8', or '.' if it is empty.
+        - values: Value in corresponding box, e.g. '8', or '123456789' if it is empty.
     """
     if len(grid) != 81:
         return {}
     rows = 'ABCDEFGHI'
     cols = '123456789'
-    return {rows[x//9]+cols[x%9]: grid[x] for x in range(81)}
+    f = lambda x: x if x != '.' else cols
+    return {rows[x//9]+cols[x%9]: f(grid[x]) for x in range(81)}
+
+
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    values_elim = values.copy()
+    for k, v in values.items():
+        if len(v) == 1:
+            for n in peers[k]:
+                values_elim[n] = values_elim[n].replace(v,'')
+    return values_elim
+
+
+def only_choice(values):
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unitlist:
+        for d in '123456789':
+            cells = [x for x in unit if d in values[x]]
+            if len(cells) == 1:
+                values[cells[0]] = d
+    return values
+
+
+def reduce_puzzle(values):
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def display(values):
     """
